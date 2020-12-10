3bca35f8936ce28dbddb02907415e6b15e2a6a08
TangRufus <tangrufus@gmail.com>
2020-12-10 22:58:45 +0000

    Add descriptions and remove comments

--- cmd/open.go
+++ cmd/open.go
@@ -58,8 +58,8 @@ const cyberduckBookmarkJ2 = `
 
 // openCmd represents the open command
 var openCmd = &cobra.Command{
-	Use:   "open",
-	Short: "Open SFTP connections to Trellis servers via Cyberduck",
+	Use:   "open [<flags>] <environment> [<site>]",
+	Short: "Open SFTP connections to Trellis servers",
 	Args:  cobra.RangeArgs(1, 2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// Ensure we are inside trellis root and activate virtualenv.
@@ -121,14 +121,4 @@ var openCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(openCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// openCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// openCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
